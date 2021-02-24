8956a3037ff044720f608ad3712c0686c212c856
Sumit Kumar <sumit1523@gmail.com>
2021-02-24 22:03:10 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 const App = () => {
 	const { t, i18n } = useTranslation();
 
-	function handleClick(lang) {
+	const changeLang= lang => {
 		i18n.changeLanguage(lang)
 	}
 
@@ -14,9 +14,9 @@ const App = () => {
 		<div className="App">
 			<header className="App-header">
 				<div>
-					<button className="button" onClick={() => handleClick('en')} > English </button>
-					<button className="button" onClick={() => handleClick('hi')} > Hindi </button>
-					<button className="button" onClick={() => handleClick('de')} > Germany </button>
+					<button className="button" onClick={() => changeLang('en')} > English </button>
+					<button className="button" onClick={() => changeLang('hi')} > Hindi </button>
+					<button className="button" onClick={() => changeLang('de')} > Germany </button>
 				</div>
 				<img src={logo} className="App-logo" alt="logo" />
 				<h3>{t('title')}</h3>
