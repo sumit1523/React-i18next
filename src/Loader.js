c832fc6d959f345db6076843f78dac61a0e3569f
Sumit Kumar <sumit1523@gmail.com>
2021-02-24 21:09:57 +0530

    loader fix

--- src/Loader.js
+++ src/Loader.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './loader.css';
 
 const Loader = () => {
-	return (
-		<div className="square_loader">
-			<div className="square">
-				<div></div>
-				<div></div>
-			</div>
+	return (	
+		<div className="square">
+			<div></div>
+			<div></div>
 		</div>
 	)
 }
